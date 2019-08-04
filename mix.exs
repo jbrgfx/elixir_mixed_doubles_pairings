b8ee14953884895b5ebd46b7a918ce62d013ca60
James B. Ross <35547276+jbrgfx@users.noreply.github.com>
2019-08-04 13:00:06 -0400

    Incremented version number

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Mixeddoubles.MixProject do
   def project do
     [
       app: :mixeddoubles,
-      version: "0.1.0",
-      elixir: "~> 1.8",
+      version: "0.2.0",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
